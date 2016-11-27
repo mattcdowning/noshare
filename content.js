8e59c6b41611668c47a955ba1d7ae1cb7588be22
mattcdowning <mattcdowning@gmail.com>
2016-11-26 19:14:08 -0500

    update extension to block more sharing widgets

--- content.js
+++ content.js
@@ -1,17 +1,82 @@
-var share = setInterval(function(){
-  if ( $('.js-postShareWidget').length){
-    $('.js-postShareWidget').hide();
-  }
-}, 100);
+// Bloomberg
+if (document.location.hostname == 'www.bloomberg.com'){
+  var bloomberg = setInterval(function(){
+    if ( $('.bb-nav-social-container').length){
+      $('.bb-nav-social-container').hide();
+      $('.social-share').hide();
+    }
+  }, 1000);
+}
 
-var postact = setInterval(function(){
-  if ( $('.postActions').length){
-    $('.postActions').hide();
-  }
-}, 100);
+// CNN
+if (document.location.hostname == 'www.cnn.com'){
+  var cnn = setInterval(function(){
+    if ( $('.js-gigya-sharebar').length){
+      $('.js-gigya-sharebar').hide();
+    }
+  }, 1000);
+}
 
-var promoCard = setInterval(function(){
-  if ( $('.promoCardWrapper').length){
-    $('.promoCardWrapper').hide();
-  }
-}, 100);
+// Huffington Post
+if (document.location.hostname == 'www.huffingtonpost.com'){
+  var huffington = setInterval(function(){
+    if ( $('.share-bar').length){
+      $('.share-bar').hide();
+    }
+  }, 1000);
+}
+
+// Lifehacker
+if (document.location.hostname == 'lifehacker.com'){
+  var lifehacker = setInterval(function(){
+    if ( $('.js_sharingfooter').length){
+      $('.js_sharingfooter').hide();
+    }
+  }, 1000);
+}
+
+// Mashable
+if (document.location.hostname == 'mashable.com'){
+  var mashable = setInterval(function(){
+    if ( $('.share-buttons').length){
+      $('.share-buttons').hide();
+      $('.shares').hide();
+    }
+  }, 1000);
+}
+
+//Medium
+
+// Share bar
+if (document.location.hostname == 'medium.com'){
+  var share = setInterval(function(){
+    if ( $('.js-postShareWidget').length){
+      $('.js-postShareWidget').hide();
+    }
+  }, 1000);
+  // Recommend/Comment bar
+  var postact = setInterval(function(){
+    if ( $('.postActions').length){
+      $('.postActions').hide();
+    }
+  }, 1000);
+
+}
+
+// Verge
+if (document.location.hostname == 'www.theverge.com'){
+  var vergeVox = setInterval(function(){
+    if ( $('.c-social-buttons').length){
+      $('.c-social-buttons').hide();
+    }
+  }, 1000);
+}
+
+// VOX
+if (document.location.hostname == 'www.vox.com'){
+  var vergeVox = setInterval(function(){
+    if ( $('.c-social-buttons').length){
+      $('.c-social-buttons').hide();
+    }
+  }, 1000);
+}
