143e33f99e176eb72321fe379583ee8f655c20da
mattcdowning <mattcdowning@gmail.com>
2017-01-22 17:14:03 -0500

    refactor to remove jquery

--- content.js
+++ content.js
@@ -1,107 +1,122 @@
 const host = document.location.hostname;
-
+//select element via class
+function select(selector) {
+     return document.querySelector(selector);
+}
+//hide element
+function hide(el){
+  el.style.display = 'none';
+}
 // Bloomberg
+const bloombergShare = select('.bb-nav-social-container')
 if (host == 'www.bloomberg.com'){
-  var bloomberg = setInterval(function(){
-    if ( $('.bb-nav-social-container').length){
-      $('.bb-nav-social-container').hide();
-      $('.social-share').hide();
+  const bloomberg = setInterval(() => {
+    if (bloombergShare){
+      hide(bloombergShare);
     }
   }, 1000);
 }
-
 // CNN
+const cnnShare = select('.js-gigya-sharebar');
 if (host == 'www.cnn.com'){
-  var cnn = setInterval(function(){
-    if ( $('.js-gigya-sharebar').length){
-      $('.js-gigya-sharebar').hide();
+  const cnn = setInterval(() => {
+    if (cnnShare){
+      hide(cnnShare);
     }
   }, 1000);
 }
-
 // Huffington Post
+const huffingtonShare = select('.share-bar');
 if (host == 'www.huffingtonpost.com'){
-  var huffington = setInterval(function(){
-    if ( $('.share-bar').length){
-      $('.share-bar').hide();
+  const huffington = setInterval(() => {
+    if (huffingtonShare){
+      hide(huffingtonShare);
     }
   }, 1000);
 }
-
 // Lifehacker
+const lifehackerShare = select('.js_sharingfooter');
 if (host == 'lifehacker.com'){
-  var lifehacker = setInterval(function(){
-    if ( $('.js_sharingfooter').length){
-      $('.js_sharingfooter').hide();
+  const lifehacker = setInterval(() => {
+    if (lifehackerShare){
+      hide(lifehackerShare);
     }
   }, 1000);
 }
-
 // Mashable
+const mashShareBtn = select('.share-buttons');
+const mashShares = select('.shares');
 if (host == 'mashable.com'){
-  var mashable = setInterval(function(){
-    if ( $('.share-buttons').length){
-      $('.share-buttons').hide();
-      $('.shares').hide();
+  const mashable = setInterval(() => {
+    if (mashShareBtn){
+      hide(mashShareBtn);
+      hide(mashShares);
+      // $('.share-buttons').hide();
+      // $('.shares').hide();
     }
   }, 1000);
 }
-
 //Medium
-
-// Share bar
+  // Share bar
+const medPostShareWidget = select('.js-postShareWidget');
+const medRecBar = select('.postActionsBar');
+const medAuthorCard = select('.promoCardWrapper');
 if (host.includes('medium.') || host.includes('medium.com') || host.includes('blog.medium.com')){
-  var share = setInterval(function(){
-    if ( $('.js-postShareWidget').length){
-      $('.js-postShareWidget').hide();
+  const medShare = setInterval(() => {
+    if (medPostShareWidget){
+      hide(medPostShareWidget);
     }
   }, 1000);
   // Recommend/Comment bar
-  var postact = setInterval(function(){
-    if ( $('.postActions').length){
-      $('.postActions').hide();
+  const postact = setInterval(() => {
+    if(medRecBar){
+      hide(medRecBar);
     }
   }, 1000);
   //Follow Author card
-  var promoCardWrapper = setInterval(function(){
-    if ( $('.promoCardWrapper').length){
-      $('.promoCardWrapper').hide();
+  const promoCardWrapper = setInterval(() => {
+    if(medAuthorCard){
+      hide(medAuthorCard);
     }
   }, 1000);
 }
-
 //The Washington Post
+const washingtonSocial = select('.social-tools-wrapper');
 if (host == 'www.washingtonpost.com'){
-  var share = setInterval(function(){
-    if ( $('.social-tools-wrapper').length){
-      $('.social-tools-wrapper').hide();
+  const washingtonShare = setInterval(() => {
+    if (washingtonSocial){
+      hide(washingtonSocial);
     }
   }, 1000);
 }
-
 //WSJ
+const wsjArticleTools = document.getElementById('article_tools');
 if (host == 'www.wsj.com'){
-  var share = setInterval(function(){
-    if ( $('#article_tools').length){
-      $('#article_tools').hide();
+  const wsjShare = setInterval(() => {
+    if (wsjArticleTools){
+      hide(wsjArticleTools)
     }
   }, 1000);
 }
-
 // Verge
+const vergeSocialBtns = select('.c-social-buttons');
+const vergeTabBar = select('.c-tab-bar');
 if (host == 'www.theverge.com'){
-  var vergeVox = setInterval(function(){
-    if ( $('.c-social-buttons').length){
-      $('.c-social-buttons').hide();
+  const verge = setInterval(() => {
+    if(vergeSocialBtns){
+      hide(vergeSocialBtns);
+      hide(vergeTabBar);
     }
   }, 1000);
 }
-
 // VOX
+const voxSocialBtns = select('.c-social-buttons');
+const voxTabBar = select('.c-tab-bar');
 if (host == 'www.vox.com'){
-  var vergeVox = setInterval(function(){
-    if ( $('.c-social-buttons').length){
-      $('.c-social-buttons').hide();
+  const vox = setInterval(() => {
+    if(voxSocialBtns){
+      hide(voxSocialBtns);
+      hide(voxTabBar);
     }
   }, 1000);
 }
